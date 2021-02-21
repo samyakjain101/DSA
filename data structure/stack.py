08483a3db48e52bb2a1bf84899f85d1e6f4dc8b5
Samyak Jain <samyakjain101@gmail.com>
2021-02-22 01:48:30 +0530

    stack

---
+++ data structure/stack.py
@@ -0,0 +1,47 @@
+class StackEmpty(Exception):
+    def __init__(self, message='Stack is Empty'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.__root = None
+        self.__length = 0
+
+    def __increase_length(self):
+        self.__length += 1
+
+    def __decrease_length(self):
+        self.__length -= 1
+
+    def __empty_check(self):
+        if self.__root is None:
+            raise StackEmpty()
+
+    def push(self, data=None):
+        if self.__root is None:
+            self.__root = Node(data=data)
+        else:
+            self.__root = Node(data=data, next=self.__root)
+
+        self.__increase_length()
+
+    def pop(self):
+        self.__empty_check()
+        self.__root = self.__root.next
+
+        self.__decrease_length()
+
+    def top(self):
+        self.__empty_check()
+        return self.__root.data
+
+    def size(self):
+        return self.__length
