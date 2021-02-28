9077cf5b0a2f0550887d063744471dd80d0e1b8a
Samyak Jain <samyakjain101@gmail.com>
2021-03-01 00:11:16 +0530

    generate all balanced paranthesis

---
+++ recursion/14_generate_all_balanced_paranthesis.py
@@ -0,0 +1,14 @@
+def generate_all_balanced_paranthesis(output, open, close):
+    if open == close == 0:
+        print(output)
+        return
+    if open != 0:
+        generate_all_balanced_paranthesis(output+'(', open-1, close)
+    if close > open:
+        generate_all_balanced_paranthesis(output+')', open, close-1)
+
+
+if __name__ == "__main__":
+    n = 3
+    output = ''
+    generate_all_balanced_paranthesis(output, open=n, close=n)
