0a098f89da55761bb9a2b63ef937172c8daa45cf
Samyak Jain <samyakjain101@gmail.com>
2021-03-13 17:26:08 +0530

    longest palindromic subsequence

---
+++ dynamic_programming/x_longest_palindromic_subsequence.py
@@ -0,0 +1,12 @@
+from q_longest_common_subsequence_bottom_up import lcs
+
+
+if __name__ == "__main__":
+    string1 = "agbcba"
+    n = len(string1)
+
+    string2 = string1[::-1]
+    m = len(string2)
+
+    longest_common_subsequence_len = lcs(string1, string2, n, m)
+    print(longest_common_subsequence_len)
