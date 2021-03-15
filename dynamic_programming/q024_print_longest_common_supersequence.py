c49af4b119abeb9e743f5fec11968588629e95f4
Samyak Jain <samyakjain101@gmail.com>
2021-03-16 03:01:49 +0530

    print longest common supersequence

---
+++ dynamic_programming/q024_print_longest_common_supersequence.py
@@ -0,0 +1,43 @@
+def lcs(string1, string2, n, m):
+
+    for i in range(n+1):
+        for j in range(m+1):
+            if string1[i-1] == string2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(
+                    dp[i-1][j],
+                    dp[i][j-1])
+
+
+if __name__ == "__main__":
+    string1 = "HELLO"
+    string2 = "GEEK"
+    n, m = len(string1), len(string2)
+    dp = [[0]*(m+1) for _ in range(n+1)]
+    lcs(string1, string2, n, m)
+
+    i, j = n, m
+    result = ''
+    while i > 0 and j > 0:
+        if string1[i-1] == string2[j-1]:
+            result += string1[i-1]
+            i -= 1
+            j -= 1
+        else:
+            if dp[i-1][j] < dp[i][j-1]:
+                result += string2[j-1]
+                j -= 1
+            else:
+                result += string1[i-1]
+                i -= 1
+
+    while i > 0:
+        result += string1[i-1]
+        i -= 1
+
+    while j > 0:
+        result += string2[j-1]
+        j -= 1
+
+    print(result[::-1])
