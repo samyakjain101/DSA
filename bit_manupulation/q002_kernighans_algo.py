deef4545b418a03e5aa1fd952fc8d52fbcbdf5bb
Samyak Jain <samyakjain101@gmail.com>
2021-03-24 13:11:45 +0530

    count number of set bits (kernighans algo)

---
+++ bit_manupulation/q002_kernighans_algo.py
@@ -0,0 +1,20 @@
+"""Problem statement
+
+https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/bit-manipulation/kernighans-algo-official/ojquestion
+"""
+
+
+def kernighans(n):
+
+    count = 0
+    while n != 0:
+        rsb = n & -n
+        n -= rsb
+        count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    n = 58
+    print(kernighans(n))
